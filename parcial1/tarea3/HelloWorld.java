6053ee9c5baf9ae10623678299c0f7c3eff74508
EspirituRA <rafaspir55hotmail.com>
2022-10-04 17:27:34 -0500

    Cambios

---
+++ parcial1/tarea3/HelloWorld.java
@@ -0,0 +1,9 @@
+// HelloWorld.java
+//Author: Rafael Espiritu Arias
+// Date: sep 27,2022
+
+class HelloWorld {
+   public static void main(String[] args) {
+       System.out.println("Hello, World!");
+    }
+}

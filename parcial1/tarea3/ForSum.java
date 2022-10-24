63c93bee33a9f4588641d60428f77e1946a576bd
EspirituRA <rafaspir55hotmail.com>
2022-10-24 17:41:14 -0500

    ForSumFactorial

---
+++ parcial1/tarea3/ForSum.java
@@ -0,0 +1,18 @@
+public class ForSum{
+    //program to find the sum of naturale numbers from 1 to 1000.
+      public static void main(String[]args) {
+
+int sum = 0;
+          double n = 4;
+      
+          // for loop
+          for (int i = 1; i <= n; ++i) {
+            // body inside for loop
+            sum += i;     // sum = sum + i
+            System.out.println(sum);
+          }
+             
+          System.out.println("Sum = " + sum);
+          System.out.println("Avg = " + sum / n);
+        }
+    }
